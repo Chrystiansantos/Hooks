626586884da3c6d7c2155644cccb18ef4b0e81a6
Chrystiansantos <chrystian_santos_silva@hotmail.com>
2020-03-20 16:29:52 -0300

    Hook useEffect => ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [tech, setTech] = useState(['React Js', 'React Native']);
@@ -7,6 +7,21 @@ function App() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+  /* Dessa maneira ele ira executar apenas uma vez */
+  useEffect(() => {
+    const storageTech = JSON.parse(localStorage.getItem('tech'));
+    if (storageTech) {
+      setTech(storageTech);
+    }
+    /* COMPONENT WillUnmount  */
+    /* Sera executada sempre que um component deixar de existir */
+    return () => {};
+  }, []);
+  /* Assim ele ira ficar monitorando o array de tech, sempre
+  que houver uma alteração ele executara */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
